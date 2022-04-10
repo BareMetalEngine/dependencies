882db0fcb8770da1cf10f812eb21ad6c20c18f99
RexDex <rexdexpl@gmail.com>
2022-04-10 08:21:20 +0200

    Updates

--- package_linux.sh
+++ package_linux.sh
@@ -98,10 +98,10 @@ run_build -l zlib deploy
 #run_build -l mbedtls deploy
 
 # configure and build mbedtls
-run_build -l lua pull
-run_build -l lua configure
-run_build -l lua build
-run_build -l lua deploy
+run_build -l embree pull
+run_build -l embree configure
+run_build -l embree build
+run_build -l embree deploy
 
 # configure and build all other libs
 #run_build configure
